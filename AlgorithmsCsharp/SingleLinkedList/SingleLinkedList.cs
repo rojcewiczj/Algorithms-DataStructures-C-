9910662dd15a2cf77d157afd89503aeebc003d0b
John Rojcewicz <rojcewiczj@gmail.com>
2020-10-29 23:41:25 -0500

    Add project files.

---
+++ AlgorithmsCsharp/SingleLinkedList/SingleLinkedList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsCsharp.SingleLinkedList
+{
+    class SingleLinkedList
+    {
+        private Node start;
+
+        public SingleLinkedList()
+        {
+            start = null;
+        }
+
+        public void Display()
+        {
+            Node p;
+            if (start == null)
+            {
+                Console.WriteLine("list is empty");
+                return;
+            }
+        }
+    }
+}
