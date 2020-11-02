c8fbf42c8a2c765ee5a407c3c0cb11d5bb940a86
John Rojcewicz <rojcewiczj@gmail.com>
2020-11-02 17:03:00 -0600

    Created a hourglass 2d class and algorithm

---
+++ AlgorithmsCsharp/Hourglass/HourGlassAdder.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+
+
+namespace AlgorithmsCsharp.Hourglass
+{
+    class HourGlassAdder
+    {
+        public int[,] data;
+        public int Max;
+        public HourGlassAdder()
+        {
+            System.Random random = new System.Random();
+            data = new int[6, 6];
+            for (int x = 0; x < 6; x++)
+            {
+                
+                for (int y = 0; y < 6; y++)
+                {
+                    data[x, y] = random.Next(0, 9);
+                    Console.Write(String.Format(" {0} ", data[x, y]));
+
+                }
+                Console.WriteLine();
+            }
+        }
+        public int FindMax()
+        {
+            List<int> sums = new List<int>();
+            for (int x = 1; x < 5; x++)
+            {
+
+                for (int y = 1; y < 5; y++)
+                {
+                    int sum = data[x - 1, y - 1] + data[x - 1, y] + data[x - 1, y + 1]
+                                             + data[x, y] +
+                                  data[x + 1, y - 1] + data[x + 1, y] + data[x + 1, y + 1];
+                    sums.Add(sum);
+
+                }
+                
+                
+            }
+            return sums.Max();
+        }
+
+    }
+}
--- AlgorithmsCsharp/Program.cs
+++ AlgorithmsCsharp/Program.cs
@@ -8,12 +8,9 @@ namespace AlgorithmsCsharp
     {
         static void Main(string[] args)
         {
-            LinkedList myList = new LinkedList();
-            myList.addToEnd(4);
-            myList.addToEnd(7);
-            myList.addToEnd(3);
-            myList.addToTheBeginning(2);
-            myList.Print();
+            Hourglass.HourGlassAdder hourglass = new Hourglass.HourGlassAdder();
+            Console.WriteLine(hourglass.FindMax());
+            
         }
     }
 }
