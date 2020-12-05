c1258df495c5e1ae4223139a1480d07e3aaf850e
John Rojcewicz <rojcewiczj@gmail.com>
2020-12-04 23:22:10 -0600

    worked on hashing function

---
+++ AlgorithmsCsharp/DataStructures/HashTable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsCsharp.DataStructures
+{
+    class HashTable
+    {
+
+
+        public static int Hash(string _string)
+        {
+            byte[] asciiBytes = Encoding.ASCII.GetBytes(_string);
+            int num_val = 0;
+
+            foreach (byte ascii in asciiBytes)
+            {
+                num_val += ascii;
+            }
+            return num_val;
+        }
+    }
+}
--- AlgorithmsCsharp/Program.cs
+++ AlgorithmsCsharp/Program.cs
@@ -17,19 +17,10 @@ namespace AlgorithmsCsharp
         static void Main(string[] args)
         {
    
-            PriorityQueue PQ = new PriorityQueue();
+          
+            Console.WriteLine(HashTable.Hash("johas"));
 
-
-            PQ.Insert(new PQNode("common cold", 5));
-            PQ.Insert(new PQNode("fever", 3));
-            PQ.Insert(new PQNode("heart attack", 1));
-            PQ.Insert(new PQNode("broken arm", 2));
-            PQ.Insert(new PQNode("chopped arm", 6));
-            PQ.Insert(new PQNode("itchy arm", 8));
            
-
-            PQ.print();
-
         }
     }
 }
