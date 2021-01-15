1fa97e9d3143e09f15f1425de18326f3343f233a
John Rojcewicz <rojcewiczj@gmail.com>
2021-01-14 23:16:35 -0600

    solved Heaters LeetCode problem

---
+++ AlgorithmsCsharp/Arrays/Heaters.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsCsharp.Arrays
+{
+     class Heaters
+    {
+        public static int[] houses = new int[] {3,5,19,30 };
+
+        public static int[] heaters = new int[] {1};
+
+        public static int minRadius()
+        {
+            int final_radius = 0;
+            Array.Sort(houses);
+            Array.Sort(heaters);
+            int i = 0;
+            int left;
+            int right;
+
+
+            foreach (int house in houses)
+            {
+                while (i < heaters.Length - 1 && heaters[i] <= house)
+                {
+                    i++;
+                }
+
+                if (i > 0)
+                {
+                    left = Math.Abs(house - heaters[i - 1]);
+                }
+                else
+                {
+                    left = Math.Abs(house - heaters[0]);
+                }
+                right = Math.Abs(heaters[i] - house);
+                final_radius = Math.Max(final_radius, Math.Min(left, right));
+
+
+            }
+
+            return final_radius;
+
+    }
+    }
+}
--- AlgorithmsCsharp/Program.cs
+++ AlgorithmsCsharp/Program.cs
@@ -13,7 +13,7 @@ namespace AlgorithmsCsharp
 
         static void Main(string[] args)
         {
-            MaxSumPermutations.Find();
+            Heaters.minRadius();
           
             
         }
