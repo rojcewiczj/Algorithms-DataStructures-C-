903b942c130760b85619a90c5a73e27fb6ee77a0
John Rojcewicz <rojcewiczj@gmail.com>
2021-02-03 23:33:19 -0600

    AlgoExpert practice c#

---
+++ AlgorithmsCsharp/Arrays/IsValidSequence.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsCsharp.Arrays
+{
+    class IsValidSequence
+    {
+
+		public static bool IsValidSubsequence(List<int> array, List<int> sequence)
+		{
+			int i = 0;
+			int j = 0;
+
+			while (j < sequence.Count && i < array.Count)
+			{
+				if (array[i] == sequence[j])
+				{
+					j++;
+				}
+				i++;
+			}
+			return j == sequence.Count;
+
+
+		}
+	}
+
+}
+
