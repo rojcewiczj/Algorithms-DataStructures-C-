52a5f9d7c4d5293ee4661f8ef6771ad844f74d22
John Rojcewicz <rojcewiczj@gmail.com>
2021-02-06 22:51:43 -0600

    binary search algo practice on AlgoExpert

---
+++ AlgorithmsCsharp/Arrays/BinarySearch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsCsharp.Arrays
+{
+    class BinarySearch
+    {
+        
+
+
+		public static int BinaryS(int[] array, int target)
+		{
+			// Write your code here.
+			int low = 0;
+			int high = array.Length - 1;
+			while (low <= high)
+			{
+				int mid = (low + high) / 2;
+				if (array[mid] == target)
+				{
+					return mid;
+				}
+				if (target > array[mid])
+				{
+					low = mid + 1;
+				}
+				else if (target < array[mid])
+				{
+					high = mid - 1;
+				}
+			}
+			return -1;
+		}
+
+
+	}
+
+
+}
