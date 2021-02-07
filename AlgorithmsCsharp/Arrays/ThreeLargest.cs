4771286f1345855ef3ff3f07b5967b8a02e3eef3
John Rojcewicz <rojcewiczj@gmail.com>
2021-02-06 23:38:11 -0600

    Algo for returning three largest intagers without sorting array

---
+++ AlgorithmsCsharp/Arrays/ThreeLargest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsCsharp.Arrays
+{
+    class ThreeLargest
+    {
+
+		public static int[] FindThreeLargestNumbers(int[] array)
+		{
+			// Write your code here
+			int[] ThreeLargest = new int[3];
+			for (int i = 0; i < array.Length; i++)
+			{
+				Update(ThreeLargest, array[i]);
+			}
+			return ThreeLargest;
+		}
+
+		public static void Update(int[] largest, int num)
+		{
+			if (num > largest[2])
+			{
+				Shift(largest, num, 2);
+			}
+			else if (num > largest[1])
+			{
+				Shift(largest, num, 1);
+			}
+			else if (num > largest[0])
+			{
+				Shift(largest, num, 0);
+			}
+
+		}
+		public static void Shift(int[] threeLargest, int num, int index)
+		{
+			for (int i = 0; i <= index; i++)
+			{
+				if (i == index)
+				{
+					threeLargest[i] = num;
+				}
+				else
+				{
+					threeLargest[i] = threeLargest[i + 1];
+				}
+			}
+
+		}
+	}
+}
+
