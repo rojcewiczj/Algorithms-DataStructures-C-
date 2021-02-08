b7b954c422c406c47cba2384a807bbeac809a7d5
John Rojcewicz <rojcewiczj@gmail.com>
2021-02-07 23:28:26 -0600

    Worked on selection sort in c#

---
+++ AlgorithmsCsharp/Arrays/SelectionSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsCsharp.Arrays
+{
+    class SelectionSort
+    {
+		public static int[] Sort(int[] array)
+		{
+			// Write your code here.
+			if (array.Length == 0)
+			{
+				return new int[] { };
+			}
+			int startIdx = 0;
+			while (startIdx < array.Length - 1)
+			{
+				int smallestIdx = startIdx;
+				for (int i = startIdx + 1; i < array.Length; i++)
+				{
+					if (array[smallestIdx] > array[i])
+					{
+						smallestIdx = i;
+					}
+				}
+				int temp = array[smallestIdx];
+				array[smallestIdx] = array[i];
+				array[i] = temp;
+			}
+			return array;
+		}
+	}
+}
