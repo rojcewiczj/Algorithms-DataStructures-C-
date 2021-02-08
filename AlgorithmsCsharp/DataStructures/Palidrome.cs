dc2524115080dc8e4c1581b24b89a3367abae287
John Rojcewicz <rojcewiczj@gmail.com>
2021-02-07 23:33:02 -0600

    Checking if a string is a palidrom c#

---
+++ AlgorithmsCsharp/DataStructures/Palidrome.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsCsharp.DataStructures
+{
+    class Palidrome
+    {
+		public static bool IsPalindrome(string str)
+		{
+			string new_string = string.Empty;
+			for (int i = str.Length - 1; i > -1; i--)
+			{
+				new_string += str[i];
+			}
+			if (str == new_string)
+			{
+				return true;
+			}
+			return false;
+		}
+	}
+}
