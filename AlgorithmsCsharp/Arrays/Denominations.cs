cf9d907289cfcd1eaad34f232af2d142e0592162
John Rojcewicz <rojcewiczj@gmail.com>
2021-02-14 22:29:29 -0600

    AlgoExpert ways to make change medium c#

---
+++ AlgorithmsCsharp/Arrays/Denominations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsCsharp.Arrays
+{
+    public class Denominations
+    {
+		public static int NumberOfWaysToMakeChange(int n, int[] denoms)
+		{
+			int[] ways = new int[n + 1];
+			ways[0] = 1;
+			foreach (int denom in denoms)
+			{
+				Console.WriteLine("denom: {0}", denom);
+				for (int amount = 1; amount < n + 1; amount++)
+				{
+					Console.WriteLine("amount: {0}", amount);
+					if (denom <= amount)
+					{
+						ways[amount] += ways[amount - denom];
+						Console.WriteLine("current ways: {0}", ways[amount]);
+					}
+				}
+			}
+			return ways[n];
+		}
+	}
+}
